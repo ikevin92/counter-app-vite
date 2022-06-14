ede715f855107f6fd1cced5364659b89a70dc6a2
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-06-13 22:47:48 -0500

    section-14

---
+++ src/HelloWorldtApp.jsx
@@ -0,0 +1,11 @@
+// export function App() {
+//   return (
+//     <div>
+//       <h1>Hello, world!!</h1>
+//     </div>
+//   )
+// }
+
+export const HelloWorldtApp = () => {
+  return <h1>HelloWorldtApp</h1>
+}
